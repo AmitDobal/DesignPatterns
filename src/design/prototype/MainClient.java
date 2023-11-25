d384c8667cb428a70444409551920f009246ee78
Amit Dobal <2019pte5188@mnit.ac.in>
2023-11-26 01:06:33 +0530

    prototype design pattern

--- src/design/prototype/MainClient.java
+++ src/design/prototype/MainClient.java
@@ -1,14 +1,27 @@
 package design.prototype;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainClient {
 	public static void main(String[] args) throws CloneNotSupportedException {
 		NetworkConnection connection = new NetworkConnection();
+		List list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		
 		connection.setIp("192.168.1.1");
 		connection.loadData();
-		System.out.println(connection);
-
-		NetworkConnection connection2 = (NetworkConnection) connection.clone();
+		connection.setDataList(list);
 		
+		list.add(3);
+		NetworkConnection connection2 = (NetworkConnection) connection.clone();
+		list.add(4);
+		list.add(5);
+		
+		connection2.getDataList().add(99);
+		
+		System.out.println(connection);
 		System.out.println(connection2);
 
 	}
--- src/design/prototype/NetworkConnection.java
+++ src/design/prototype/NetworkConnection.java
@@ -1,8 +1,11 @@
 package design.prototype;
 
+import java.util.List;
+
 public class NetworkConnection implements Cloneable {
 	private String ip;
 	private String data;
+	private List dataList;
 
 	public String getIp() {
 		return ip;
@@ -19,12 +22,21 @@ public class NetworkConnection implements Cloneable {
 	public void setData(String data) {
 		this.data = data;
 	}
+	
+
+	public List getDataList() {
+		return dataList;
+	}
+
+	public void setDataList(List dataList) {
+		this.dataList = dataList;
+	}
 
 	@Override
 	public String toString() {
-		return "NetworkConnection [Ip=" + ip + ", data=" + data + "]";
+		return "NetworkConnection [ip=" + ip + ", data=" + data + ", dataList=" + dataList + "]";
 	}
-	
+
 	public String loadData(){
 		System.out.println("Loading data.......");
 		try {
